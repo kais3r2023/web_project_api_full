a820bd7bb6973a378cb31588177d0c2836043e94
kais3r2023 <122757568+kais3r2023@users.noreply.github.com>
2024-07-16 00:09:41 -0500

    problemas CORS

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const users = require('./routes/users');
-const cards = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
-const url = 'mongodb://localhost:27017/aroundb';
-const { jwtMiddleware } = require('./middleware/auth');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const users = require("./routes/users");
+const cards = require("./routes/cards");
+const { login, createUser } = require("./controllers/users");
+const url = "mongodb://localhost:27017/aroundb";
+const { jwtMiddleware } = require("./middleware/auth");
 const { requestLogger, errorLogger } = require("./middleware/logger");
-const {errors} = require("celebrate")
-const whiteList = [ 'https://kais3r.chickenkiller.com', 'http://localhost:3000'];
+const { errors } = require("celebrate");
+const cors = require("cors");
+const whiteList = ["http://kais3r.chickenkiller.com", "http://localhost:3000"];
 
-
-
-mongoose.connect(url)
-
+mongoose.connect(url);
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }))
-
-var cors = require('cors');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Cors
 app.use(
   cors({
     origin: whiteList,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
   })
 );
-app.options('*', cors());
-
-
 
 app.use(requestLogger);
-app.post('/signin', login);
-app.post('/signup', createUser);
-
+app.post("/signin", login);
+app.post("/signup", createUser);
 
 //Rutas protegidas
-app.use('/users', jwtMiddleware, users);
-app.use('/cards', jwtMiddleware, cards);
-
+app.use("/users", jwtMiddleware, users);
+app.use("/cards", jwtMiddleware, cards);
 
 app.use(errorLogger);
 
-app.use(errors());// controlador de errores de celebrate
+app.use(errors()); // controlador de errores de celebrate
 
 // Ruta por defecto: devuelve un mensaje de error para cualquier otra ruta
 app.use((req, res) => {
-  res.status(404).json({ message: 'Recurso solicitado no encontrado' });
+  res.status(404).json({ message: "Recurso solicitado no encontrado" });
 });
 
 app.listen(PORT, () => {
